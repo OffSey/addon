162b9482b015e82e198b86e16c85c862f0481e27
OffSey <87211817+OffSey@users.noreply.github.com>
2025-01-15 21:11:44 +0100

    Update sv_rtxthemepark.lua

--- addon/server/sv_rtxthemepark.lua
+++ addon/server/sv_rtxthemepark.lua
@@ -3,43 +3,18 @@ if Config.ThemeParkRTX then
     RegisterNetEvent("rtx_themepark:Global:UsingAttractionPlayer")
     AddEventHandler("rtx_themepark:Global:UsingAttractionPlayer", function(themeparkhandlerusing)
         local playersource = source
-        if themeparkhandlerusing then
-            local result, errorText = exports[Config.ResourceName]:SetTempPermission(playersource, "Client", "BypassNoclip", true, false)
+        local bypass = {
+            {category = "Client", permission = "BypassNoclip"},
+            {category = "Misc", permission = "BypassSpoofedWeapons"},
+            {category = "Vehicle", permission = "BypassBulletproofTires"}
+        }
+        for k, v in pairs(bypass) do
+            
+            local result, errorText = exports[Config.ResourceName]:SetTempPermission(playersource, v.category, v.permission, themeparkhandlerusing, false)
             if not result then
-                print("[fiveguard] Error for approved the RTX_ThemePark NoClip permissions for " .. playersource .. ": " .. errorText)
+                print("[Fiveguard] Error for permissions for RTX_ThemePark for the permissions: "..v.permission.. " PlayerID: "..playersource.." Error Text: "..errorText)
             else
-                print("[fiveguard] Permissions for RTX_ThemePark Noclip approved for " .. playersource)
-            end
-            local result, errorText = exports[Config.ResourceName]:SetTempPermission(playersource, "Misc", "BypassSpoofedWeapons", true, false)
-            if not result then
-                print("[fiveguard] Error for approved the RTX_ThemePark SpoofedWeapons permissions for " .. playersource .. ": " .. errorText)
-            else
-                print("[fiveguard] Permissions for RTX_ThemePark SpoofedWeapons approved for " .. playersource)
-            end
-            local result, errorText = exports[Config.ResourceName]:SetTempPermission(playersource, "Vehicle", "BypassBulletproofTires", true, false)
-            if not result then
-                print("[fiveguard] Error for approved the RTX_ThemePark BulletproofTires permissions for " .. playersource .. ": " .. errorText)
-            else
-                print("[fiveguard] Permissions for RTX_ThemePark BulletproofTires approved for " .. playersource)
-            end
-        else
-            local result, errorText = exports[Config.ResourceName]:SetTempPermission(playersource, "Client", "BypassNoclip", false, false)
-            if not result then
-                print("[fiveguard] Error for removed the RTX_ThemePark NoClip permissions " .. playersource .. ": " .. errorText)
-            else
-                print("[fiveguard] Permissions for approved RTX_ThemePark Noclip for " .. playersource)
-            end
-            local result, errorText = exports[Config.ResourceName]:SetTempPermission(playersource, "Misc", "BypassSpoofedWeapons", false, false)
-            if not result then
-                print("[fiveguard] Error for removed the RTX_ThemePark SpoofedWeapons permissions for " .. playersource .. ": " .. errorText)
-            else
-                print("[fiveguard] Permissions for RTX_ThemePark SpoofedWeapons removed for " .. playersource)
-            end
-            local result, errorText = exports[Config.ResourceName]:SetTempPermission(playersource, "Vehicle", "BypassBulletproofTires", false, false)
-            if not result then
-                print("[fiveguard] Error for removed the RTX_ThemePark BulletproofTires permissions for " .. playersource .. ": " .. errorText)
-            else
-                print("[fiveguard] Permissions for RTX_ThemePark BulletproofTires removed for " .. playersource)
+                print("[Fiveguard] Permissions for RTX_ThemePark for the permissions: "..v.permission.." its: "..themeparkhandlerusing.. "PlayerID: "..playersource)
             end
         end
     end)
