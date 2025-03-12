73137dc725e5f95a175a8f402a46584d74f60099
OffSey <87211817+OffSey@users.noreply.github.com>
2025-03-12 19:02:32 +0100

    Add files via upload

---
+++ client/cl_anticarry.lua
@@ -0,0 +1,31 @@
+local data = LoadResourceFile(CurrentResourceName,'config.lua')
+local Config = assert(load(data))()?.AntiCarry
+while not Fiveguard do Wait(0) end
+if not Config?.enable then return end
+
+local function isPlayingBlacklistedAnim(ped)
+    if IsEntityPlayingAnim(ped, 'anim@mp_rollarcoaster', 'hands_up_idle_a_player_one', 3) then
+        return true
+    end
+    return false
+end
+
+local function isWhitelistedZone(ped)
+    local playerCoords = GetEntityCoords(ped)
+    for i = 1, #Config.whitelistedZones do
+        local distance = #(playerCoords - zone.coords)
+        if distance <= Config.whitelistedZones[i].radius then
+            return true
+        end
+    end
+    return false
+end
+
+local function checking()
+    local playerPed = PlayerPedId()
+    if isPlayingBlacklistedAnim(playerPed) and not isWhitelistedZone(playerPed) then
+        TriggerServerEvent("fg:addon:antiThrow")
+    end
+    Citizen.SetTimeout(200, checking)
+end
+Citizen.SetTimeout(200, checking)
---
+++ server/sv_rtx_theme_park.lua
@@ -0,0 +1,21 @@
+local data = LoadResourceFile(CurrentResourceName,'config.lua')
+local Config = assert(load(data))()?.RTX_ThemePark_Bypass
+while not Fiveguard do Wait(0) end
+if GetResourceState(Fiveguard) ~= 'started' then return end
+if not Config?.enable then return end
+
+RegisterNetEvent("rtx_themepark:Global:UsingAttractionPlayer", function(state)
+    local bypass = {
+        {category = "Client", permission = "BypassNoclip"},
+        {category = "Misc", permission = "BypassSpoofedWeapons"},
+        {category = "Vehicle", permission = "BypassBulletproofTires"}
+    }
+    for i = 1, #bypass do
+        local result, errorText = exports[Fiveguard]:SetTempPermission(source, bypass[i].category, bypass[i].permission, state, false)
+        if not result then
+            Error(("[RTX Theme Park] Can't give temporany permission!\nPermission: ^5\"%s.%s\"^0\nPlayer: ^5[%s] %s^0\nReason: ^5%s^0"):format(v.category,v.permission,source,GetPlayerName(source),errorText))
+        else
+            Debug(("[RTX Theme Park] temporany Permission ^5\"%s.%s\"^0 was %s succesfully!\nPlayer changed: ^5[%s] %s^0"):format(v.category,v.permission,state == true and"^2granted^0" or "^1removed^0",source,GetPlayerName(source)))
+        end
+    end
+end)
