b5a25d6e8f6be2627e0b4ac5baf512f7d42a0185
OffSey <87211817+OffSey@users.noreply.github.com>
2025-03-12 19:02:16 +0100

    Delete addon directory

--- addon/client/cl_rcore.lua
+++
@@ -1,8 +0,0 @@
-if Config.RcoreClothing then
-    AddEventHandler('rcore_clothing:onClothingShopOpened', function()
-        TriggerServerEvent("rcore_clothing:fiveguard:stealoutfit", true)
-    end)
-    AddEventHandler('rcore_clothing:onClothingShopClosed', function()
-        TriggerServerEvent("rcore_clothing:fiveguard:stealoutfit", false)
-    end)
-end
