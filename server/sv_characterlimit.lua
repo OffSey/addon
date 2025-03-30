17ca27cf26473ed50ef444f30a2038e71de672b9
OffSey <87211817+OffSey@users.noreply.github.com>
2025-03-30 20:47:40 +0200

    Update sv_characterlimit.lua

--- server/sv_characterlimit.lua
+++ server/sv_characterlimit.lua
@@ -1,1 +1,14 @@
+local data = LoadResourceFile(CurrentResourceName, 'config.lua')
+local Config = assert(load(data))()?.LimitCharacterName
+while not Fiveguard do Wait(0) end
+if GetResourceState(Fiveguard) ~= 'started' then return end
+if not Config?.enable then return end
 
+function isCharacterNameValid(name)
+    local nameLength = #name
+    if nameLength > Config.CharacterLimit then
+        local excess = nameLength - Config.CharacterLimit
+        return false, "You have " .. excess .. " extra characters. Limit: " .. Config.CharacterLimit
+    end
+    return true, ""
+end
