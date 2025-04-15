05aa5f659d69a1695b2cbd6cd404b8b259a5c3d2
OffSey <87211817+OffSey@users.noreply.github.com>
2025-04-15 02:36:40 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,7 +5,7 @@ return {
     -- prevent cheaters to stop client side of this resource
     Heartbeat = {
         enable = true,
-        timeOut = 10, -- How long after the difference with os.time is the ban? (If not, in how many seconds is the ban (10 seconds is fine))
+        timeOut = 30, -- How long after the difference with os.time is the ban? (If not, in how many seconds is the ban (30 seconds is fine))
         ThreadTime = 5, -- Thread Time (5 seconds is fine)
         ban = true
     },
