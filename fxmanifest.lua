27a703de7d9843ca1d935e06dedbdf5559b596e9
OffSey <87211817+OffSey@users.noreply.github.com>
2025-06-23 20:59:17 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version 'cerulean'
 game 'gta5'
-version "1.3.5"
+version "1.3.6"
 lua54 'yes'
 author 'Offsey & Jeakels discord.gg/fiveguard'
 description 'Addon pack. Anti-CarryVehicle/Anti-Stopper/Rtx-ThemePark Fix/Custom Ace Permissions/TxAdmin Permissions - Name management (whitelist string/limit character) for fiveguard'
