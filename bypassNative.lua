733381dc04e0242a51477fa86c6e85001f05c74d
Jeakels <136957358+jeakels@users.noreply.github.com>
2025-08-22 16:58:29 +0200

    Update bypassNative.lua

--- bypassNative.lua
+++ bypassNative.lua
@@ -1,21 +1,18 @@
 local a1 = GetCurrentResourceName()
 if a1 == CurrentResourceName then return end
-print("[^2INFO^0] the module ^5bypassNative^0 by addon is loaded into "..tostring(a1).." successfully!")
 if IsDuplicityVersion() then
+    print("[^2INFO^0] the module ^5bypassNative^0 by addon is loaded into "..tostring(a1).." successfully!")
     local setEntityCoords = SetEntityCoords
     SetEntityCoords = function (entity,...)
-        print('SV>>SetEntityCoords',entity,...)
         local source = NetworkGetEntityOwner(entity)
         exports[Fiveguard]:SetTempPermission(source,"Client","BypassTeleport",true)
         setEntityCoords(entity,...)
         Citizen.SetTimeout(100,function ()
             exports[Fiveguard]:SetTempPermission(source,"Client","BypassTeleport",false)
         end)
-        return
     end
     local setEntityVisible = SetEntityVisible
     SetEntityVisible = function (entity,toogle,...)
-        print('SV>>SetEntityVisible',entity,toogle,...)
         local source = NetworkGetEntityOwner(entity)
         exports[Fiveguard]:SetTempPermission(source,"Client","BypassInvisible",not toogle)
         return setEntityVisible(entity,toogle,...)
@@ -25,17 +22,14 @@ if IsDuplicityVersion() then
 else
     local setEntityCoords = SetEntityCoords
     SetEntityCoords = function (entity,...)
-        print('CL>>SetEntityCoords',entity,...)
         TriggerServerEvent("fg:addon:SetTempPermission:BypassTeleport",true,a1)
         setEntityCoords(entity,...)
         Citizen.SetTimeout(1000,function ()
             TriggerServerEvent("fg:addon:SetTempPermission:BypassTeleport",false,a1)
         end)
-        return
     end
     local setEntityVisible = SetEntityVisible
     SetEntityVisible = function (entity,toogle,...)
-        print('CL>>SetEntityVisible',entity,toogle,...)
         TriggerServerEvent("fg:addon:SetTempPermission:BypassInvisible",not toogle,a1)
         return setEntityVisible(entity,toogle,...)
     end
