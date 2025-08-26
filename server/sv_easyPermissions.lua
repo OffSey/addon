488f34be96ba38213e19c66bb901f7af63ba7db8
Jeakels <136957358+jeakels@users.noreply.github.com>
2025-08-27 01:09:20 +0200

    Dev (#14)
    * Add weapon information in ban
    
    * Update sv_nativePermissions.lua
    
    * using format instead of concatenation
    
    * Add ped information in ban reason
    
    * Update config.lua
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update sv_main.lua
    
    * Update sv_weaponProtection.lua
    
    * Fixed false positives for not boolean values on hasItem
    
    * Fixed error when success is not provided
    
    * Fixed a typo
    
    * Update sv_main.lua
    
    * Versioning
    
    * Removing test commands
    
    * Update README.md
    
    * Fixed error in console
    
    * Default config
    
    * Cleaning if palyer left
    
    * Warn more verbose
    
    * Update sv_easyPermissions.lua
    
    * Update sv_main.lua
    
    * Update sv_main.lua
    
    * Update sv_weaponProtection.lua
    
    * Update sv_nativePermissions.lua
    
    * Update sv_main.lua

--- server/sv_easyPermissions.lua
+++ server/sv_easyPermissions.lua
@@ -8,7 +8,7 @@ if Config.txAdminPermissions.enable or Config.AcePermissions.enable or Config.Fr
     if (IsPrincipalAceAllowed('resource.'..CurrentResourceName,'command.add_ace')) then
         Debug(('The resource ^5%s^0 have permission to manage ACE permissions, good!'):format(CurrentResourceName))
     else
-        Error(('%s dosen\'t have permission to manage ace permission!\nAdd ^3add_ace resource.%s command allow^1 in your server.cfg'):format(CurrentResourceName,CurrentResourceName))
+        Error(("%s does not have permission to manage ace permission!\nAdd ^3add_ace resource.%s command allow^1 in your server.cfg"):format(CurrentResourceName,CurrentResourceName))
         return
     end
     if (Config.txAdminPermissions.enable and Config.AcePermissions.enable) or (Config.AcePermissions.enable and Config.FrameworkPermissions.enable) or (Config.txAdminPermissions.enable and Config.FrameworkPermissions.enable) then
@@ -98,15 +98,6 @@ if Config.AcePermissions.enable then
             end
         end
     end)
-
-    RegisterCommand('gpp', function (source,args)
-        for group,_ in pairs(Config.AcePermissions.groups) do
-            if IsPlayerAceAllowed(source, 'has.'..group) then
-                SetPermission(source, group, true)
-                break
-            end
-        end
-    end,false)
 
     RegisterNetEvent('fg:addon:playerSpawned', function()
         Debug('[fg:addon:playerSpawned]', source)
